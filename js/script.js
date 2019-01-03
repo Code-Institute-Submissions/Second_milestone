9d9752748bcaab6562d1903e375bdbafdee1cff9
danielsky <karma0rg4u@gmail.com>
2019-01-03 01:12:53 +0000

    Add another API to show tidal data in a form of a line chart using the 3D.js. Add line chart to the innerHTML

--- js/script.js
+++ js/script.js
@@ -6,26 +6,26 @@ window.addEventListener('load', function(event) {
 // Surfing locations definition
 
 var surfSpots = [
-    {title: 'Portrush', county: 'Antrim', location: {lat: 55.170241, lng: -6.731873}, point: 0, map: 'north'},
-    {title: 'Magheroarty', county: 'Donegal', location: {lat: 55.162985, lng: -8.143272}, point: 0, map: 'north'},
-    {title: 'Falcarragh', county: 'Donegal', location: {lat: 55.16073, lng: -8.085937}, point: 0, map: 'north'},
-    {title: 'Rossnowlagh', county: 'Donegal', location: {lat: 54.555689, lng: -8.210135}, point: 270, map: 'west'},
-    {title: 'Bundoran', county: 'Donegal', location: {lat: 54.492515, lng: -8.268799}, point: 315, map: 'west'},
-    {title: 'Easky', county: 'Sligo', location: {lat: 54.291575, lng: -8.955840}, point: 0, map: 'west'},
-    {title: 'Inishcrone', county: 'Sligo', location: {lat: 54.211954, lng: -9.102001}, point: 0, map: 'west'},
-    {title: 'Keel', county: 'Mayo', location: {lat: 53.973033, lng: -10.07223}, point: 225, map: 'west'},
-    {title: 'Dun Loughan', county: 'Galway', location: {lat: 53.419440, lng: -10.148626}, point: 225, map: 'west'},
-    {title: 'Lahinch', county: 'Clare', location: {lat: 52.933675, lng: -9.350100}, point: 225, map: 'west'},
-    {title: 'Doonbeg', county: 'Clare', location: {lat: 52.76445, lng: -9.493389}, point: 270, map: 'west'},
-    {title: 'Castlegregory', county: 'Kerry', location: {lat: 52.276666, lng: -10.037212}, point: 315, map: 'west'},
-    {title: 'Inch Strand', county: 'Kerry', location: {lat: 52.137499, lng: -9.979695}, point: 225, map: 'west'},
-    {title: 'Barley Cove', county: 'Cork', location: {lat: 51.468823, lng: -9.775531}, point: 180, map: 'south'},
-    {title: 'Inchydoney', county: 'Cork', location: {lat: 51.595535, lng: -8.861236}, point: 135, map: 'south'},
-    {title: 'Castlefreake', county: 'Cork', location: {lat: 51.557116, lng: -8.966045}, point: 225, map: 'south'},
-    {title: 'Garretstown', county: 'Cork', location: {lat: 51.640028, lng: -8.564953}, point: 225, map: 'south'},
-    {title: 'Tramore', county: 'Waterford', location: {lat: 52.156210, lng: -7.127300}, point: 180, map: 'south'},
-    {title: 'Magheramore', county: 'Wicklow', location: {lat: 52.930841, lng: -6.023053}, point: 135, map: 'east'},
-    {title: 'Whiterock', county: 'Dublin', location: {lat: 53.265934, lng: -6.106232}, point: 135, map: 'east'}
+    {title: 'Portrush', county: 'Antrim', location: {lat: 55.170241, lng: -6.731873}, point: 0, map: 'north', station: '%22Malin%20Head%20-%20Portmore%20Pier%22'},
+    {title: 'Magheroarty', county: 'Donegal', location: {lat: 55.162985, lng: -8.143272}, point: 0, map: 'north', station: '%22Aranmore%20Island%20-%20Leabgarrow%22'},
+    {title: 'Falcarragh', county: 'Donegal', location: {lat: 55.16073, lng: -8.085937}, point: 0, map: 'north', station: '%22Aranmore%20Island%20-%20Leabgarrow%22'},
+    {title: 'Rossnowlagh', county: 'Donegal', location: {lat: 54.555689, lng: -8.210135}, point: 270, map: 'west', station: '%22Killybegs%20Port%22'},
+    {title: 'Bundoran', county: 'Donegal', location: {lat: 54.492515, lng: -8.268799}, point: 315, map: 'west', station: '%22Killybegs%20Port%22'},
+    {title: 'Easky', county: 'Sligo', location: {lat: 54.291575, lng: -8.955840}, point: 0, map: 'west', station: '%22Sligo%22'},
+    {title: 'Inishcrone', county: 'Sligo', location: {lat: 54.211954, lng: -9.102001}, point: 0, map: 'west', station: '%22Sligo%22'},
+    {title: 'Keel', county: 'Mayo', location: {lat: 53.973033, lng: -10.07223}, point: 225, map: 'west', station: '%22Ballyglass%20Harbour%22'},
+    {title: 'Dun Loughan', county: 'Galway', location: {lat: 53.419440, lng: -10.148626}, point: 225, map: 'west', station: '%22OPW%20Rossaveel%20Pier%22'},
+    {title: 'Lahinch', county: 'Clare', location: {lat: 52.933675, lng: -9.350100}, point: 225, map: 'west', station: '%22OPW%20Rossaveel%20Pier%22'},
+    {title: 'Doonbeg', county: 'Clare', location: {lat: 52.76445, lng: -9.493389}, point: 270, map: 'west', station: '%22OPW%20Rossaveel%20Pier%22'},
+    {title: 'Castlegregory', county: 'Kerry', location: {lat: 52.276666, lng: -10.037212}, point: 315, map: 'west', station: '%22Dingle%20Harbour%22'},
+    {title: 'Inch Strand', county: 'Kerry', location: {lat: 52.137499, lng: -9.979695}, point: 225, map: 'west', station: '%22Dingle%20Harbour%22'},
+    {title: 'Barley Cove', county: 'Cork', location: {lat: 51.468823, lng: -9.775531}, point: 180, map: 'south', station: '%22Castletownbere%20Port%22'},
+    {title: 'Inchydoney', county: 'Cork', location: {lat: 51.595535, lng: -8.861236}, point: 135, map: 'south', station: '%22OPW%20Ringaskiddy%22'},
+    {title: 'Castlefreake', county: 'Cork', location: {lat: 51.557116, lng: -8.966045}, point: 225, map: 'south', station: '%22OPW%20Ringaskiddy%22'},
+    {title: 'Garretstown', county: 'Cork', location: {lat: 51.640028, lng: -8.564953}, point: 225, map: 'south', station: '%22OPW%20Ringaskiddy%22'},
+    {title: 'Tramore', county: 'Waterford', location: {lat: 52.156210, lng: -7.127300}, point: 180, map: 'south', station: '%22Dunmore%20East%20Harbour%22'},
+    {title: 'Magheramore', county: 'Wicklow', location: {lat: 52.930841, lng: -6.023053}, point: 135, map: 'east', station: '%22OPW%20Arklow%20Harbour%22'},
+    {title: 'Whiterock', county: 'Dublin', location: {lat: 53.265934, lng: -6.106232}, point: 135, map: 'east', station: '%22Dublin%20Port%22'}
 ] 
 
 // Google Maps API
@@ -242,13 +242,13 @@ function searchPoint(nameKey, myArray){
     }
 };
 
-// function searchDirection(nameKey, myArray){
-//     for (var i=0; i < myArray.length; i++) {
-//         if (myArray[i].map === nameKey) {
-//             return myArray[i].map;
-//         }
-//     }
-// };
+function searchStation(nameKey, myArray){
+    for (var i=0; i < myArray.length; i++) {
+        if (myArray[i].title === nameKey) {
+            return myArray[i].station;
+        }
+    }
+};
 
 var spotForecast = document.getElementById('surfingSpots').addEventListener('click', function(e) {
     console.log(e.target.childNodes[0].data);
@@ -572,10 +572,207 @@ var spotForecast = document.getElementById('surfingSpots').addEventListener('cli
                         return check();
                     };
 
+
+        // ****************************************************************** Marine Institute of Ireland data
+
+        var timeToday = new Date();
+        // Adding second digit for day and month < 10
+        
+        function addZero(n) {
+            return n < 10 ? '0' + n : '' + n;
+        };
+        
+        var timeTodayFormat = timeToday.getFullYear() + '-' + addZero(timeToday.getMonth()) + '-' + addZero(timeToday.getDate());
+
+        var timeTomorrow = timeToday.setDate(timeToday.getDate() +1);
+
+        // Unix time to local time conversion
+
+        function timeConverter(t) {
+            var tmr = new Date(t);
+            var year = tmr.getFullYear();
+            var month = addZero(tmr.getMonth());
+            var day = addZero(tmr.getDate());
+            var time = year + '-' + month + '-' + day;
+            return time; 
+        }
+
+        function timeConvDayMonth(t) {
+            var tmr = new Date(t);
+            var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            var month = monthNames[tmr.getMonth()];
+            var day = tmr.getDate();
+            var time = day + ' ' + month;
+            return time; 
+        }
+
+        var timeFrom = timeTodayFormat + 'T00%3A00%3A00Z';
+        var timeTo = timeConverter(timeTomorrow) + 'T00%3A00%3A00Z';
+        var stationId = searchStation(e.target.childNodes[0].data, surfSpots);
+
+        var xhr = new XMLHttpRequest();
+
+        xhr.open('GET', `https://erddap.marine.ie/erddap/tabledap/IMI-TidePrediction.json?time%2CstationID%2CWater_Level_ODM&time%3E=${timeFrom}&time%3C=${timeTo}&stationID%3E=${stationId}`, true);
+
+        xhr.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var data = JSON.parse(this.responseText);
+                // console.log(data);
+                // console.log(data.table.rows[0][2] + ' + ' + data.table.rows[10][2]);
+
+                // D3.js DATA
+
+                var tidePrediction = data.table.rows[0][2];
+                // console.log(tidePrediction);
+
+                var tidesTime = [];
+                var tidesValue = [];
+                var tidesTimeValue = {};
+
+                var delta = 10;
+                // console.log(data.table.rows.length);
+                // console.log(delta);
+
+                // for (i = 0; i < data.table.rows.length; i = i + delta) {
+                //     tidesTime.push(data.table.rows[i][0]);
+                // }
+
+                for (i = 0; i < 240; i = i + delta) {
+                    tidesTime.push(data.table.rows[i][0]);
+                }
+
+                // for (i = 0; i < data.table.rows.length; i = i + delta) {
+                //     tidesValue.push(data.table.rows[i][2]);
+                // }
+
+                for (i = 0; i < 240; i = i + delta) {
+                    tidesValue.push(data.table.rows[i][2]);
+                }
+
+                tidesTime.forEach(function (time, i) {
+                    return tidesTimeValue[time] = tidesValue[i];
+                });
+
+                // console.log(tidesTime[10]);
+
+                // UTC time to Hour and Minutes
+
+                var time = new Date(tidesTime[10]);
+                var timeHM = time.getUTCHours() + ':' + time.getUTCMinutes();
+
+                // console.log(timeHM);
+            
+                // console.log(typeof(tidesValue));
+                // console.log(Math.max(tidesValue));
+
+                // the tidesValue index reflect the time as well since the first element (i.e index 0) equal the midnight 00:00
+
+                // console.log(tidesTimeValue);
+
+                var dataApi = [];
+
+                for (i = 0; i < 240; i = i + delta) {
+                    dataApi.push (
+                        {
+                            // date: data.table.rows[i][0],
+                            date: (new Date(data.table.rows[i][0])).getUTCHours(),
+                            value: data.table.rows[i][2]
+                        });
+                };
+
+                // console.log(dataApi);
+
+        // LINE CHART
+
+                /* implementation heavily influenced by http://bl.ocks.org/1166403 */
+                
+                // define dimensions of graph
+                var m = [10, 10, 10, 10]; // margins
+                var w = 350 - m[1] - m[3]; // width
+                var h = 150 - m[0] - m[2]; // height
+                
+                // create a simple data array that we'll plot with a line (this array represents only the Y values, X will just be the index location)
+                // var data = [-3, -6, -2, 0, 5, 2, 0, -3, -8, -9, -2, 5, 9, 13];
+                var data = tidesValue;
+
+                // X scale will fit all values from data[] within pixels 0-w
+                var x = d3.scaleLinear().domain([0, tidesValue.length]).range([0, w]);
+                // Y scale will fit values from 0-10 within pixels h-0 (Note the inverted domain for the y-scale: bigger is up!)
+                var y = d3.scaleLinear().domain([d3.min(tidesValue), d3.max(tidesValue)]).range([h, 0]);
+                    // automatically determining max range can work something like this
+                    // var y = d3.scale.linear().domain([0, d3.max(data)]).range([h, 0]);
+
+                // create a line function that can convert data[] into x and y points
+                var line = d3.line()
+                    // assign the X function to plot our line as we wish
+                    .x(function(d,i) { 
+                        // verbose logging to show what's actually being done
+                        // console.log('Plotting X value for data point: ' + d + ' using index: ' + i + ' to be at: ' + x(i) + ' using our xScale.');
+                        // return the X coordinate where we want to plot this datapoint
+                        return x(i); 
+                    })
+                    .y(function(d) { 
+                        // verbose logging to show what's actually being done
+                        // console.log('Plotting Y value for data point: ' + d + ' to be at: ' + y(d) + " using our yScale.");
+                        // return the Y coordinate where we want to plot this datapoint
+                        return y(d); 
+                    })
+                    .curve(d3.curveBasis)
+
+                    // Add an SVG element with the desired dimensions and margin.
+                    var graph = d3.select("#tides").append("svg:svg")
+                        .attr("width", w + m[1] + m[3])
+                        .attr("height", h + m[0] + m[2])
+                        .append("svg:g")
+                        .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+
+                    // create yAxis
+                    var xAxis = d3.axisBottom().scale(x).ticks(4).tickSize(-h);
+                    // Add the x-axis.
+                    graph.append("svg:g")
+                        .attr("class", "x axis")
+                        .attr("transform", "translate(0," + h + ")")
+                        .call(xAxis);
+
+
+                    // // create left yAxis
+                    // var yAxisLeft = d3.axisLeft().scale(y);
+                    // // Add the y-axis to the left
+                    // graph.append("svg:g")
+                    //       .attr("class", "y axis")
+                    //       .attr("transform", "translate(-10,0)")
+                    //       .call(yAxisLeft);
+                    
+                    // Add the line by appending an svg:path element with the data line we created above
+                    // do this AFTER the axes above so that the line is above the tick-lines
+                    graph.append("svg:path").attr("d", line(data));
+
+                    graph.append("line")
+                    //   .attr("x1",-6)
+                        .attr("y1",y(0))//so that the line passes through the y 0
+                        .attr("x2",w)
+                        .attr("y2",y(0))//so that the line passes through the y 0
+                        .style("stroke", "black")
+                        .style("opacity", ".5");
+
+            } else if (this.readyState == 4 && this.status == 402) {
+                document.getElementById('tides').innerHTML = 'Data request exceeded! Please come back tomorrow';
+            }  
+        };
+
+        xhr.onerror = function() {
+            console.log('Request error');
+        };
+
+        xhr.send(); 
+
                 console.log('Button clicked: ' + e.target.childNodes[0].data + ' ' + lat + ' ' + lng);
                 console.log('OpenWeather API forecast spot, location and date: ' + openWeatherApiData.name + ' ' + openWeatherApiData.coord.lat + ' ' + openWeatherApiData.coord.lon + ' ' + unixToLocal(openWeatherApiData.dt));
                 console.log('OpenWeatherAPI 3days forecast times:' +openWeatherExApiData.list[5].dt_txt + ' ' + openWeatherExApiData.list[13].dt_txt + ' ' + openWeatherExApiData.list[21].dt_txt);
                 console.log('StormGlassAPI spot: ' + weather.meta.lat + ' ' + weather.meta.lng);
+                console.log('Tides Chart from: ' + timeFrom);
+                console.log('Tides Chart to: ' + timeTo);
+                console.log('Tides Station ID: ' + searchStation(e.target.childNodes[0].data, surfSpots));
 
                 forecast.innerHTML = 
                 '<div class="row text-center">' +
@@ -630,6 +827,9 @@ var spotForecast = document.getElementById('surfingSpots').addEventListener('cli
                             '<div class="col-xs-4">' + '<h3>' + '&#8451' + '</h3>' + '<h3>' + 'air | water' + '</h3>' + '</div>' +
                             '<div class="col-xs-4">' + '<h2>' + afternoonAverage.waterTemperature + '</h2>' + '</div>' +
                         '</div>' +
+                    '</div>' +
+
+                    '<div class="col-xs-12" id="tides">' +
                     '</div>' +
 
                     '</div>' +
